8c4384915cdfa2e4161e5476f67f244d70c6f6bc
ogm710811 <ogm710811@gmail.com>
2017-07-12 13:11:33 -0400

    new logo and favicon

--- src/app/services/session.service.ts
+++ src/app/services/session.service.ts
@@ -6,19 +6,28 @@ import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class SessionService {
-  private sessionUrl: string;
+  private sessionUrl: string = 'http://localhost:3000/api.stem'
+  private sessionParams: string;
 
   constructor( private http: Http ) { }
 
-   signup (user) {
-    this.sessionUrl = 'http://localhost:3000/api.stem/signup';
+  signup (user) {
+    this.sessionParams = '/signup';
 
-    const theOriginalPromise = this.http.post(this.sessionUrl, user).toPromise();
+    const theOriginalPromise = this.http.post( this.sessionUrl + this.sessionParams, user).toPromise();
     
     const theParsedPromise = theOriginalPromise.then((result) => {
       return result.json();
     });
     
     return theParsedPromise;
+  }
+
+  isLoggedIn () {
+    this.sessionParams = '/loggedin';
+    
+    return this.http.get( this.sessionUrl + this.sessionParams )
+      .toPromise()
+      .then(result => result.json());
   }
 }
